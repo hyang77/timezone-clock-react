584cfcff5f0eb6015b58dc1438e389711952e009
Haoli Yang <hyang77@hawk.iit.edu>
2021-04-17 23:08:33 -0500

    Create basic html skeleton and css style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">Timezone Clock</h1>
+      <div>
+        <select name="timezone">
+          <option value="utc-12">UTC-12</option>
+          <option value="utc-11">UTC-11</option>
+          <option value="utc-10">UTC-10</option>
+          <option value="utc-9">UTC-9</option>
+          <option value="utc-8">UTC-8</option>
+          <option value="utc-7">UTC-7</option>
+          <option value="utc-6">UTC-6</option>
+          <option value="utc-5">UTC-5</option>
+          <option value="utc-4">UTC-4</option>
+          <option value="utc-3">UTC-3</option>
+          <option value="utc-2">UTC-2</option>
+          <option value="utc-1">UTC-1</option>
+          <option value="utc">UTC</option>
+          <option value="utc+1">UTC+1</option>
+          <option value="utc+2">UTC+2</option>
+          <option value="utc+3">UTC+3</option>
+          <option value="utc+4">UTC+4</option>
+          <option value="utc+5">UTC+5</option>
+          <option value="utc+6">UTC+6</option>
+          <option value="utc+7">UTC+7</option>
+          <option value="utc+8">UTC+8</option>
+          <option value="utc+9">UTC+9</option>
+          <option value="utc+10">UTC+10</option>
+          <option value="utc+11">UTC+11</option>
+          <option value="utc+12">UTC+12</option>
+        </select>
+        <button type="button">Add</button>
+      </div>
+      <div className="card-container">
+        <div className="time-card">
+          <p className="timezone">UTC</p>
+          <p>16:00:25</p>
+          <button type="button">Delete</button>
+        </div>
+      </div>
     </div>
   );
 }
