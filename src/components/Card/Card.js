efd82b8c9291ce61ed94b9ad5f3a75806b3ab349
Haoli Yang <hyang77@hawk.iit.edu>
2021-04-18 22:11:06 -0500

    Set up UTC timer

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,16 +1,46 @@
 import React from "react";
 import "./Card.css";
 
-function Card(props) {
-  return (
-    <div className="time-card">
-      <p className="timezone">{props.timezone}</p>
-      <p>16:00:25</p>
-      <button type="button" onClick={() => props.removeClock(props.timezone)}>
-        Delete
-      </button>
-    </div>
-  );
+class Card extends React.Component {
+  constructor(props) {
+    super(props);
+    this.updateTime = this.updateTime.bind(this);
+    this.state = { hours: "", minutes: "", seconds: "" };
+  }
+  componentDidMount() {
+    this.timer = setInterval(this.updateTime, 1000);
+  }
+
+  updateTime() {
+    this.setState({
+      hours: new Date().getUTCHours(),
+      minutes: new Date().getUTCMinutes(),
+      seconds: new Date().getUTCSeconds(),
+    });
+    console.log("hi");
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  render() {
+    const { hours, minutes, seconds } = this.state;
+    return (
+      <div className="time-card">
+        <p className="timezone">{this.props.timezone}</p>
+        <p>
+          {hours}:{minutes}:{seconds}
+        </p>
+        <button
+          type="button"
+          onClick={() => this.props.removeClock(this.props.timezone)}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Card;
